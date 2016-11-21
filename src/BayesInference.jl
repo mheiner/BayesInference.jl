311a4db105098a53168ab213a08a776d88c6a780
Matt Heiner <mheiner@ucsc.edu>
2016-11-21 15:51:16 -0800

    added Distributions to REQUIRE and logsumexp function

--- src/BayesInference.jl
+++ src/BayesInference.jl
@@ -1,5 +1,30 @@
 module BayesInference
 
-# package code goes here
+
+"""
+LogSumExp trick
+    logsumexp(x)
+
+Computes `log(sum(exp(x)))` in a stable manner.
+
+# Example
+```julia
+  x = rand(5)
+  logsumexp(x)
+  log(sum(exp(x)))
+```
+"""
+function logsumexp(x::Array{Float64}, usemax::Bool=true)
+  if usemax
+    m = maximum(x)
+  else
+    m = minimum(x)
+  end
+
+  m + log(sum(exp(x - m)))
+end
+
+
+
 
 end # module
