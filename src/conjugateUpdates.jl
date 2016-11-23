f713d4a26d9136b7d2da24a3fb030decc04e8340
Matt Heiner <mheiner@ucsc.edu>
2016-11-22 22:42:20 -0800

    condNorm function

--- src/conjugateUpdates.jl
+++ src/conjugateUpdates.jl
@@ -1,4 +1,6 @@
 """
+conjugateUpdates.jl
+
 The function naming convention for this file is:
 post_likelihood_unknownParams
 """
@@ -10,7 +12,7 @@ export post_norm_mean;
 """
     post_norm_mean(ȳ, n, σ2, μ0, v0)
 
-  Returns conjugate normal posterior under independent normal loglikelihood
+  Returns conjugate normal posterior under independent normal likelihood
   with known variance and normal prior on the mean.
 
   Accepts the sample mean and returns a normal distribution.
@@ -20,11 +22,35 @@ function post_norm_mean(ȳ::Float64, n::Int, σ2::Float64, μ0::Float64, v0::Fl
   v0 > 0.0 || throw(ArgumentError("v0 must be positive."))
   n > 0.0 || throw(ArgumentError("n must be positive."))
 
-  n = convert(Float64, n)
+  const n = convert(Float64, n)
 
-  v1 = 1.0 / (1.0/v0 + n/σ2)
-  sd1 = sqrt(v1)
-  μ1 = v1 * (μ0/v0 + n*ȳ/σ2)
+  const v1 = 1.0 / (1.0/v0 + n/σ2)
+  const sd1 = sqrt(v1)
+  const μ1 = v1 * (μ0/v0 + n*ȳ/σ2)
 
   Normal(μ1, sd1)
+end
+
+
+"""
+    post_norm_var(ss, n, a0, b0)
+
+  Returns conjugate inverse-gamma posterior for variance under
+  independent normal likelihood with known mean and
+  inverse-gamma prior on the variance.
+
+  Accepts `ss```= ∑(y_i - μ_i)^2`` and sample size `n`.
+
+  `a0` is the shape parameter of the inverse-gamma prior.
+  `b0` is the scale parameter of the inverse-gamma prior.
+"""
+function post_norm_var(ss::Float64, n::Int64, a0::Float64, b0::Float64)
+  a0 > 0.0 || throw(ArgumentError("a0 must be positive."))
+  b0 > 0.0 || throw(ArgumentError("b0 must be positive."))
+
+  const n = convert(Float64, n)
+  const a1 = a0 + 0.5*n
+  const b1 = b0 + 0.5*ss
+
+  InverseGamma(a1, b1)
 end
--- src/generalTools.jl
+++ src/generalTools.jl
@@ -55,3 +55,61 @@ function rDirichlet(α::Array{Float64,1}, logscale::Bool=false)
 
 out
 end
+
+
+
+"""
+    condNorm(μx, μy, Σxx, Σxy, Σyy, x)
+
+  Returns the conditional distribution of a the ``y`` subvector
+  of ``(x,y) ∼ MultivariateNormal``.
+
+  ### Example
+  ```julia
+    xx = randn(10, 5)
+    Sig = xx'xx
+    mu = collect(1:5)*1.0
+    x_indx = 1:3
+    y_indx = 4:5
+    μx = mu[x_indx]
+    μy = mu[y_indx]
+    Σxx = Sig[x_indx, x_indx]
+    Σyy = Sig[y_indx, y_indx]
+    Σxy = Sig[x_indx, y_indx]
+    x = [0.3, 2.1, 3.4]
+    @time condNorm(μx, μy, Σxx, Σxy, Σyy, x)
+  ```
+"""
+function condNorm(μx::Union{Float64, Vector{Float64}},
+  μy::Union{Float64, Vector{Float64}},
+  Σxx::Union{Float64, Array{Float64, 2}},
+  Σxy::Union{Float64, Vector{Float64}, Array{Float64, 2}},
+  Σyy::Union{Float64, Array{Float64, 2}}, x::Union{Float64, Vector{Float64}})
+
+  isposdef(Σxx) || throw(ArgumentError("Σxx must be positive definite."))
+  isposdef(Σyy) || throw(ArgumentError("Σyy must be positive definite."))
+
+  # Cholesky method (about 2 times faster than direct method)
+  U = chol(Σxx)
+
+  C = U' \ Σxy
+  D = U' \ (x - μx)
+
+  μ = μy + C'D
+  Σ = Σyy - C'C
+
+  # Direct method
+  # A = Σxx \ (x - μx)
+  # B = Σxx \ Σxy
+  #
+  # μ = μy + Σxy'A
+  # Σ = Σyy - Σxy'B
+  # Σ = 0.5 * (Σ' + Σ)
+
+  # because univariate normal parameterized with stdev
+  if length(μ) == 1
+    Σ = sqrt(Σ)
+  end
+
+  out = MultivariateNormal(μ, Σ)
+end
